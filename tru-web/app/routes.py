9ec81aa2f9425a873e8fc3ccda004130aea55fe1
Dylan Epstein-Gross <de7281@princeton.edu>
2023-10-22 00:21:16 -0400

    Added new endpoints

--- tru-web/app/routes.py
+++ tru-web/app/routes.py
@@ -16,14 +16,21 @@ GLOBAL_ID = 1
 def get_bio(transcript):
     return "No bio yet!"
 
+
+
 @app.route('/update_item', methods=['POST'])
 def update_item():
     file = request.files['audio']
-    data = file.read()
-    audio = AudioSegment.from_file(BytesIO(data))
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file:
-        audio.export(temp_file, format='mp3')
-        audioTranscript = openai.Audio.transcribe("whisper-1", temp_file)
+    new_audioTranscript = None
+    new_audioData = None
+    if file != None:
+        data = file.read()
+        audio = AudioSegment.from_file(BytesIO(data))
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file:
+            audio.export(temp_file, format='mp3')
+            audioTranscript = openai.Audio.transcribe("whisper-1", temp_file)
+        new_audioTranscript = audioTranscript["text"]
+        new_audioData = data
     
 
     new_subject = request.form.get("subject")
@@ -32,9 +39,10 @@ def update_item():
     new_pronouns = request.form.get("pronouns")
     new_briefSummary = request.form.get("briefSummary")
     new_birthYear = request.form.get("birthYear")
-    new_audioTranscript = audioTranscript["text"]
+    new_biography = request.form.get("biography")
+
     # new_bioId = request.form.get("bioId")
-    new_bioId = '65347d191ebfd94f92903b80'
+    new_bioId = 1 # <-- FAKE
 
     items_collection = mongo_db.biographies
     past_items = items_collection.find({'_id': new_bioId}).sort({'date': -1})
@@ -42,31 +50,110 @@ def update_item():
 
     print(newest_item)
 
+    old_subject = newest_item["subject"]
+    old_authorName = newest_item["authorName"]
+    old_relationship = newest_item["relationship"]
+    old_pronouns = newest_item["pronouns"]
+    old_briefSummary = newest_item["briefSummary"]
+    old_birthYear = newest_item["birthYear"]
+    old_audioTranscript = newest_item["audioTranscript"]
+    old_audioData = newest_item["audioData"]
+    old_biography = newest_item["biography"]
 
 
 
-    # old_subject = request.form.get("subject")
-    # old_authorName = request.form.get("authorName")
-    # old_relationship = request.form.get("relationship")
-    # old_pronouns = request.form.get("pronouns")
-    # old_briefSummary = request.form.get("briefSummary")
-    # old_birthYear = request.form.get("birthYear")
-    # old_audioTranscript = audioTranscript["text"]
-    # old_bioId = request.form.get("GLOBAL_ID")
+    if new_subject == None:
+        new_subject = old_subject
+    if new_authorName == None:
+        new_authorName = old_authorName
+    if new_relationship == None:
+        new_relationship = old_relationship
+    if new_pronouns == None:
+        new_pronouns = old_pronouns
+    if new_briefSummary == None:
+        new_briefSummary = old_briefSummary
+    if new_birthYear == None:
+        new_birthYear = old_birthYear
+    if new_audioTranscript == None:
+        new_audioTranscript = old_audioTranscript
+    if new_audioData == None:
+        new_audioData = old_audioData
+    
 
 
     # items_collection = mongo_db.biographies
 
-    # result = items_collection.insert_one({ 'subject': subject,
-    #                                       'authorName': authorName,
-    #                                       'relationship': relationship,
-    #                                       'pronouns': pronouns,
-    #                                       'briefSummary': briefSummary,
-    #                                       'birthYear': birthYear,
-    #                                       'audioData': data,
-    #                                       'audioTranscript': })
+    result = items_collection.insert_one({ 'subject': new_subject,
+                                          'authorName': new_authorName,
+                                          'relationship': new_relationship,
+                                          'pronouns': new_pronouns,
+                                          'briefSummary': new_briefSummary,
+                                          'birthYear': new_birthYear,
+                                          'audioData': new_audioData,
+                                          'audioTranscript': new_audioTranscript,
+                                          'biography': new_biography,
+                                          'date': datetime.datetime.now(),
+                                          'bioId': new_bioId})
     
-    return index()
+    data_point = items_collection.find({'_id': result.inserted_id})
+    inserted = (list(data_point)[0])
+    # print(inserted)
+    print(inserted['_id'])
+    return json.loads(json_util.dumps(inserted))
+
+@app.route('/add_meta', methods=['POST'])
+def add_meta():
+    global GLOBAL_ID
+
+    subject = request.form.get("subject")
+    authorName = request.form.get("authorName")
+    relationship = request.form.get("relationship")
+    pronouns = request.form.get("pronouns")
+    briefSummary = request.form.get("briefSummary")
+    birthYear = request.form.get("birthYear")
+
+    items_collection = mongo_db.biographies  # "items" is the name of the collection in MongoDB
+
+
+    result = items_collection.insert_one({'subject': subject,
+                                          'authorName': authorName,
+                                          'relationship': relationship,
+                                          'pronouns': pronouns,
+                                          'briefSummary': briefSummary,
+                                          'birthYear': birthYear,
+                                          'bioId': GLOBAL_ID})
+
+    GLOBAL_ID += 1
+
+    return result.inserted_id
+
+@app.route('/add_audio', methods=['POST'])
+def add_audio():
+
+    file = request.files['audio']
+    inserted_id = request.form.get('inserted_id')
+
+    data = file.read()
+    audio = AudioSegment.from_file(BytesIO(data))
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file:
+        audio.export(temp_file, format='mp3')
+        audioTranscript = openai.Audio.transcribe("whisper-1", temp_file)
+
+
+    items_collection = mongo_db.biographies  # "items" is the name of the collection in MongoDB
+
+    my_biography = get_bio(audioTranscript)
+
+    result = items_collection.update_one({'_id': inserted_id}, {'$set': {'audioData': data, 
+                                                                         'audioTranscript': audioTranscript,
+                                                                         'biography': my_biography,
+                                                                         'date': datetime.datetime.now()}})
+
+    data_point = items_collection.find({'_id': result.inserted_id})
+    inserted = (list(data_point)[0])
+    # print(inserted)
+    print(inserted['_id'])
+    return json.loads(json_util.dumps(inserted))
 
 
 # Adding Data to MongoDB
@@ -113,68 +200,7 @@ def add_item():
     print(inserted['_id'])
     return json.loads(json_util.dumps(inserted))
     
-    # if result.acknowledged:
-    #     return jsonify({"message": "Item added successfully", "item_id": str(result.inserted_id)})
-    # else:
-    #     return jsonify({"error": "Failed to add item"}, 500)
 
-    # file_2 = request.files['audio']
-    # print(file)
-    # try:
-    #     data = file.read()
-    #     print(data)
-    #     # audioTranscript = openai.Audio.transcribe("whisper-1", file)
-    #     # print(audioTranscript)
-    # except Exception as e:
-    #     print("Cannot be read!")
-    #     print(e)
-
-
-    # try:
-    #     audio = AudioSegment.from_file(BytesIO(data))
-
-    #     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file:
-    #         audio.export(temp_file, format="mp3")
-    #         print(temp_file)
-    #         audioTranscript = openai.Audio.transcribe("whisper-1", temp_file)
-    #     print(audioTranscript)
-    # except Exception as e:
-    #     print("Cannot be transcribed!")
-    #     print(e)
-
-    # data = request.get_json()
-    # subject = data['subject']
-    # birthYear = data['birthYear']
-    # relationship = data['relationship']
-    # authorName = data['authorName']
-    # briefSummary = data['briefSummary']
-    # audioBlob = data['audio']
-    # print("BLOB")
-    # print(audioBlob)
-    # print("\n\n")
-    # print("TYPE")
-    # print(type(audioBlob))
-    # print("\n\n")
-    # audioTranscript = openai.Audio.transcribe("whisper-1", audioBlob)
-    # print("TRANSCRIPT")
-    # print(audioTranscript)
-    # print("\n\n")
-    # audioData = audioBlob.read()
-    # print("DATA")
-    # print(audioData)
-    # print("\n\n")
-
-    #return index()
-    
-    # items_collection = mongo_db.biographies  # "items" is the name of the collection in MongoDB
-
-
-    # result = items_collection.insert_one(new_item)
-    
-    # if result.acknowledged:
-    #     return jsonify({"message": "Item added successfully", "item_id": str(result.inserted_id)})
-    # else:
-    #     return jsonify({"error": "Failed to add item"}, 500)
 
 # Querying Data from MongoDB
 @app.route('/get_all_items', methods=["GET"])
